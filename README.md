5ab48557eae19a769df1d497d4651b04d78b2404
Pedro Laxe <pedrolaxe@gmail.com>
2022-04-06 00:26:16 -0300

    Update
    Add Queries and mutations
    nodemon added

--- README.md
+++ README.md
@@ -10,8 +10,45 @@ Endpoint: http://localhost:4000/graphql
 
 Line 2 is added but in apolloServer 3 disabled the graphql playground.
 
-Running:
+Running dev mode:
 
+```sh
+yarn dev
 ```
-node src/index.js
+
+### Graphql Queries and Mutations examples
+
+Query users example:
+
+```graphql
+query{
+  users{
+    name
+    email
+    password
+    active
+  }
+}
+```
+
+Query user by Email example:
+
+```graphql
+query{
+  getUserbyEmail(email: "pedro@laxe.dev"){
+    name
+    email
+  }
+}
+```
+
+Mutation to create new user example:
+
+```graphql
+mutation{
+  createUser(name: "Jonas", email: "jonas@laxe.dev", password: "123456"){
+    name
+    email
+  }
+}
 ```
--- src/index.js
+++ src/index.js
@@ -2,15 +2,75 @@ const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
 const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        password: String!
+        active: Boolean!
+    }
+    type Posts{
+        id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
     type Query {
         hello: String
+        users: [User!]!
+        getUserbyEmail(email: String!): User!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!, password: String!): User!
     }
 `;
+const users = [
+    {
+        id: (Math.random()),
+        name: 'John Doe',
+        email: 'john@doe.com.br',
+        password: 'e10adc3949ba59abbe56e057f20f883e',
+        active: false,
+    },
+    {
+        id: (Math.random()),
+        name: 'Pedro Laxe',
+        email: 'pedro@laxe.dev',
+        password: 'e10adc3949ba59abbe56e057f20f883e',
+        active: true,
+    },
+    {
+        id: (Math.random()),
+        name: 'Vinicius',
+        email: 'vini@laxe.dev',
+        password: 'e10adc3949ba59abbe56e057f20f883e',
+        active: true,
+    }
+]
 const resolvers = {
     Query: {
-        hello: () => 'Hello World!'
+        hello: () => 'Hello World!',
+        users: () => users,
+        getUserbyEmail: (_, args) => {
+            return users.find((user) => user.email === args.email);
+        }
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                id: (Math.random()),
+                name: args.name,
+                email: args.email,
+                password: args.password,
+                active: true
+            }
+            users.push(newUser);
+            return newUser;
+        }
     }
-};
+}
 
 const server = new ApolloServer({ 
     typeDefs,
